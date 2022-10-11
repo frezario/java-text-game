0d6706840cedf11b22072a7bdb4c51aba50b1792
frezario <91615650+frezario@users.noreply.github.com>
2022-10-11 20:58:28 +0300

    Update GameManager.java

--- lotr/GameManager.java
+++ lotr/GameManager.java
@@ -7,7 +7,7 @@ public class GameManager {
         System.out.println("Player1: " + c1.toString());
         System.out.println("Player2: " + c2.toString());
         int turn = 0;
-        while (c1.isAlive() && c2.isAlive()) {
+        while (c1.isAlive() && c2.isAlive() && turn < 1000) {
             if (turn % 2 == 0) {
                 System.out.println("Player1(" + c1.getClass().getSimpleName() + ") hits Player2!");
                 c1.kick(c2);
@@ -19,6 +19,10 @@ public class GameManager {
                 System.out.println("Player1(" + c1.getClass().getSimpleName() + ") has " + c1.hp + " health-points");
             }
             turn++;
+        }
+
+        if (turn == 1000) {
+            System.out.println("The game is endless...");
         }
 
         if (turn == 1) {
